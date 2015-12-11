6684e1bf39956a45ddea6240ca001640e15b777e
Laura <lazeru08@gmail.com>
2015-12-11 09:51:16 +0100

    First commit

---
+++ app/src/main/java/dev/laz/qkeycounter/MainActivity.java
@@ -0,0 +1,16 @@
+package dev.laz.qkeycounter;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Main Activity.
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
